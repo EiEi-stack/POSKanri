d49a990b4b1626498a35e9e592ed27ed503aa55a
EiEi-stack <yccvenusstudent@gmail.com>
2020-10-30 23:22:24 +0900

    ナビゲーションドロワーを作成す

--- app/src/main/java/et/ad/poskanri/MainActivity.kt
+++ app/src/main/java/et/ad/poskanri/MainActivity.kt
@@ -3,18 +3,39 @@ package et.ad.poskanri
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-lateinit var btnReload: Button
+    lateinit var btnReload: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnReload =findViewById(R.id.btn_reload)
-        btnReload.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this,SplashScreenActivity::class.java)
-            startActivity(intent)
-        })
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close)
+        drawer.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            android.R.id.home->{
+                drawer.openDrawer(GravityCompat.START)
+                true
+            }
+            else->super.onOptionsItemSelected(item)
+        }
     }
 }
