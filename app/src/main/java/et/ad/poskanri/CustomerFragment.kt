a228ec029987c7ee780e30cbbb3f47a692fbe28f
EiEi-stack <yccvenusstudent@gmail.com>
2021-01-22 20:11:16 +0900

    円グラフを作成する

--- app/src/main/java/et/ad/poskanri/CustomerFragment.kt
+++ app/src/main/java/et/ad/poskanri/CustomerFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import kotlinx.android.synthetic.main.fragment_customer.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +32,8 @@ class CustomerFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
@@ -34,7 +41,35 @@ class CustomerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer, container, false)
+        val view= inflater.inflate(R.layout.fragment_customer, container, false)
+        val pieChart = view.findViewById<PieChart>(R.id.pieChartData)
+
+        val pieEntries= arrayListOf<PieEntry>()
+
+        pieEntries.add(PieEntry(30.0f))
+        pieEntries.add(PieEntry(40.0f))
+        pieEntries.add(PieEntry(35.0f))
+
+        pieChart.animateXY(1000,1000)
+        val pieDataSet = PieDataSet(pieEntries,"This is Pie Chart Label")
+        pieDataSet.setColors(
+            resources.getColor(R.color.colorAccent),
+            resources.getColor(R.color.colorPrimaryDark),
+            resources.getColor(R.color.colorPrimary)
+
+        )
+        val pieData = PieData(pieDataSet)
+        pieChart.centerText ="This is center Text"
+        pieChart.setCenterTextColor(resources.getColor(android.R.color.holo_red_dark))
+        pieChart.setCenterTextSize(15f)
+        pieChart.legend.isEnabled= false
+        pieChart.description.isEnabled=false
+        pieChart.description.text="This is test Description"
+        pieChart.holeRadius=75f
+
+        pieData.setDrawValues(true)
+        pieChart.data = pieData
+        return view
     }
 
     companion object {
